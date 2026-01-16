e51081ebc30a3bcfbc2c47fe5af362336a681db5
epplatero-plateroanalytics <epplatero@gmail.com>
2026-01-16 22:09:33 +0000

    correção 21

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from core.cleaner import limpar_planilha
 from core.utils import detectar_tipos
 from core.insights import gerar_kpis, gerar_insights
 from core.dashboard.filters import aplicar_filtros
-from core.export.excel_exporter import exportar_excel
-from email.email_sender import enviar_email_com_anexo
+from core.email.excel_exporter import exportar_excel
+from core.email.email_sender import enviar_email_com_anexo
 from pdf.pdf_engine_cloud import gerar_pdf
 
 
